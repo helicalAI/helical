ff93d24ed52cbf62769cc28ac6d439be4e58fb0f
Benoit Putzeys <benoit@helical-ai.com>
2024-02-25 20:37:31 +0100

    Adjust readme and example on how to use the helical package

--- README.md
+++ README.md
@@ -7,13 +7,7 @@ Then install the package `helical-package` like so
 pip install git+https://github.com/helicalAI/helical-package.git
 ```
 
-Copy the contents below in a python file `helical-test-package.py`
-```
-from helical.preprocessor import Preprocessor 
-Preprocessor().save_ensemble_mapping('./21iT009_051_full_data.csv', './ensemble_to_display_name_batch_macaca.pkl')
-```
-
-Finally, execute like so:
+Copy the file `helical-package-test.py` into your repo. As the downloading steps are one-time only, you can comment those lines out as you seem fit.Execute like so:
 ```
 python helical-test-package.py
 ```
---
+++ helical-package-test.py
@@ -0,0 +1,17 @@
+from helical.preprocessor import Preprocessor
+from helical.downloader import Downloader
+from helical.constants.enums import LoggingType
+
+if __name__ == "__main__":
+    downloader = Downloader(loging_type=LoggingType.CONSOLE)
+    downloader.get_ensemble_mapping('./data/21iT009_051_full_data.csv', './data/ensemble_to_display_name_batch_macaca.pkl')
+    downloader.download_via_link("./data/33l_8ep_1024t_1280.torch", "https://figshare.com/ndownloader/files/43423236")
+
+    preprocessor = Preprocessor(loging_type=LoggingType.CONSOLE)
+    preprocessor.transform_table(input_path='/Users/bputzeys/Documents/Helical/ETS_data/21iT009_051_full_data.csv', 
+                                 output_path='./data/full_cells_macaca.h5ad',
+                                 mapping_path='./data/ensemble_to_display_name_batch_macaca.pkl',
+                                 count_column='rcnt')
+    preprocessor.generate_tiledb_soma(input_path='./data/full_cells_macaca.h5ad',
+                                      tiledb_folder_name='./data/macaca',
+                                      measurement_name='RNA')
