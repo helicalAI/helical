c291111a14119759d16c3f1046fdd7f1ee8881d3
Benoit Putzeys <benoit@helical-ai.com>
2024-03-01 10:04:12 +0100

    Add Maxime as author

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version=__version__,
 
     url='https://github.com/helicalAI/helical-package.git',
-    author='Benoit Putzeys',
-    author_email='benoit@helical-ai.com',
+    author='Benoit Putzeys, Maxime Allard',
+    author_email='benoit@helical-ai.com, maxime@helical-ai.com',
     packages=find_packages(),
     install_requires=[
         'requests',
