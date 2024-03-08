d6646eb50d10730b24b624d10a449eab8c8dc1ee
Benoit Putzeys <benoit@helical-ai.com>
2024-03-08 22:27:27 +0100

    Quick and dirty demo of how swagger api can be used to get-embeddings from helical-package

---
+++ django/backend/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Helical API",
+        default_version='Beta',
+        description="Helical API description",
+        contact=openapi.Contact(email="benoit@helical-ai.com"),
+        license=openapi.License(name="Awesome License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('swagger.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
