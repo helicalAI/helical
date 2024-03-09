b5b2dc5fc9ed905aad3db518051f2881de1f1647
Benoit Putzeys <benoit@helical-ai.com>
2024-03-09 14:24:22 +0100

    Add Dockerfile and update README and requirements

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Prerequisites
+# Helical Package
+## Prerequisites
 
 Have the `21iT009_051_full_data.csv` file in a base repo.
 Then install the package `helical-package` like so
@@ -11,3 +12,19 @@ Copy the file `helical-package-test.py` into your repo. As the downloading steps
 ```
 python helical-test-package.py
 ```
+
+# Django backend
+This is still work-in-progress but you can already run django locally.
+First, install the requirements with 
+```
+pip install -r requirements.txt
+```
+You can then go into the django folder and run these commands
+```
+cd django
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver
+```
+In your browser, go to `http://127.0.0.1:8000/swagger/`
+To use the helical package with these endpoints in django, make sure you have the helical package installed in your environment.
--- helical-package-test.py
+++ helical-package-test.py
@@ -1,24 +1,22 @@
 from helical.models.uce import UCE
 from helical.models.sc_gpt import SCGPT
 from pathlib import Path
-
-if __name__ == "__main__":
     
-    uce = UCE()
-    
-    model_path = uce.get_model()
-    processed_data = uce.process_data(Path('<your/absolute/path/goes/here>'))
-    result = uce.run(model_path, processed_data, "macaca_fascicularis")
-    embeddings = uce.get_embeddings(result)
+uce = UCE()
 
-    print(embeddings.shape)
+model_path = uce.get_model()
+processed_data = uce.process_data(Path('<your/absolute/path/goes/here>'))
+result = uce.run(model_path, processed_data, "macaca_fascicularis")
+embeddings = uce.get_embeddings(result)
 
-    # WIP but general idea: Have different models at disposition to run inference
-    # scgpt = SCGPT()
-    
-    # scgpt.get_model()
-    # scgpt.run("macaca_fascicularis")
-    # embeddings = scgpt.get_embeddings()
+print(embeddings.shape)
 
-    # print(embeddings.shape)
+# WIP but general idea: Have different models at disposition to run inference
+# scgpt = SCGPT()
+
+# scgpt.get_model()
+# scgpt.run("macaca_fascicularis")
+# embeddings = scgpt.get_embeddings()
+
+# print(embeddings.shape)
 
