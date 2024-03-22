95d4105eb797bfd228fb3a8cfefe30303b214541
Benoit Putzeys <benoit@helical-ai.com>
2024-03-22 15:30:41 +0100

    Update ipynb to be compatible with new structure

--- helical-package-test.py
+++ helical-package-test.py
@@ -7,7 +7,7 @@ accelerator = Accelerator(project_dir=data_config["dir"])
 
 uce = UCE()
 
-model_path = uce.get_model(model_config, data_config, files_config, accelerator=accelerator)
+model = uce.get_model(model_config, data_config, files_config, accelerator=accelerator)
 ann_data = ad.read_h5ad("./data/full_cells_macaca.h5ad")
 data_loader = uce.process_data(ann_data)
 embeddings = uce.get_embeddings(data_loader)
