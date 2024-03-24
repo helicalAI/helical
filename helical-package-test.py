37c044ffbf5da0f40925095946e5ac9fea34b528
Benoit Putzeys <benoit@helical-ai.com>
2024-03-24 14:45:21 +0100

    Convert config file to json

--- helical-package-test.py
+++ helical-package-test.py
@@ -1,14 +1,17 @@
 from helical.models.uce.uce import UCE
-from uce_config import model_config, files_config, data_config
+import json
 from accelerate import Accelerator
 import anndata as ad
 from pathlib import Path
-accelerator = Accelerator(project_dir=data_config["dir"])
 import pandas as pd
 
 uce = UCE()
 
-model = uce.get_model(model_config, data_config, files_config, accelerator=accelerator)
+with open('uce_config.json') as f:
+    config = json.load(f)
+
+accelerator = Accelerator(project_dir=config["data_config"]["dir"])
+model = uce.get_model(config["model_config"], config["data_config"],  config["files_config"], accelerator=accelerator)
 df = pd.read_csv(Path('/Users/bputzeys/Documents/Helical/Code Repos/helical-package/out.csv'))
 ann_data = ad.AnnData(X = df)
 data_loader = uce.process_data(ann_data)
