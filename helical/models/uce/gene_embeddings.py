736c8f988e3828669d0efbf2b19a523b1ee15424
Benoit Putzeys <benoit@helical-ai.com>
2024-04-05 16:03:23 +0200

    Renaming final anndata to filtered_...
    making it easier to understand what happens.
    - Include logger messages.
    - Remove unsed species_to_gene_embeddings variable
      Varibale unsed in uce_utils process_data func
    - Add dict with species as key and gene symbols/
      names as values in a list

--- helical/models/uce/gene_embeddings.py
+++ helical/models/uce/gene_embeddings.py
@@ -6,7 +6,8 @@ from pathlib import Path
 from typing import Dict, Tuple
 import torch
 from scanpy import AnnData
-
+import logging
+logger = logging.getLogger(__name__)
 
 def get_gene_embedding_paths(embedding_path: Path):
     return {
@@ -27,18 +28,17 @@ def get_gene_embedding_paths(embedding_path: Path):
 # MODEL_TO_SPECIES_TO_GENE_EMBEDDING_PATH["ESM2"].update(extra_species) # adds new species
 
 
-def load_gene_embeddings_adata(adata: AnnData, species: list, embedding_model: str, embeddings_path: Path) -> Tuple[AnnData, 
-                                                                                                                   Dict[str, torch.FloatTensor],
-                                                                                                                   Dict[str, torch.FloatTensor]]:
-    """Loads gene embeddings for all the species/genes in the provided data.
+def load_gene_embeddings_adata(adata: AnnData, species: list, embedding_model: str, embeddings_path: Path) -> Tuple[AnnData, Dict[str, list[str]]]:
+    """
+    Loads gene embeddings for all the species/genes in the provided data.
 
-    :param data: An AnnData object containing gene expression data for cells.
+    :param adata: An AnnData object containing gene expression data for cells.
     :param species: Species corresponding to this adata
-    
     :param embedding_model: The gene embedding model whose embeddings will be loaded.
+    :param embedding_model: The embedding paths for the model
     :return: A tuple containing:
                - A subset of the data only containing the gene expression for genes with embeddings in all species.
-               - A dictionary mapping species name to the corresponding gene embedding matrix (num_genes, embedding_dim).
+               - A dictionary mapping species name to the names of genes.
     """
     # Get species names
     species_names = species
@@ -51,6 +51,7 @@ def load_gene_embeddings_adata(adata: AnnData, species: list, embedding_model: s
 
     # Ensure embeddings are available for all species
     if not (species_names_set <= available_species):
+        logger.error(f'Missing gene embeddings here: {embeddings_path}')
         raise ValueError(f'The following species do not have gene embeddings: {species_names_set - available_species}')
     # Load gene embeddings for desired species (and convert gene symbols to lower case)
     species_to_gene_symbol_to_embedding = {
@@ -61,21 +62,26 @@ def load_gene_embeddings_adata(adata: AnnData, species: list, embedding_model: s
         for species in species_names
     }
 
+    logger.info(f'Finished loading gene embeddings for {species_names_set} from {embeddings_path}')
+
     # Determine which genes to include based on gene expression and embedding availability
     genes_with_embeddings = set.intersection(*[
         set(gene_symbol_to_embedding)
         for gene_symbol_to_embedding in species_to_gene_symbol_to_embedding.values()
     ])
     genes_to_use = {gene for gene in adata.var_names if gene.lower() in genes_with_embeddings}
+    
     # Subset data to only use genes with embeddings
-    adata = adata[:, adata.var_names.isin(genes_to_use)]
-    # Set up dictionary mapping species to gene embedding matrix (num_genes, embedding_dim)
-    species_to_gene_embeddings = {
-        species_name: torch.stack([
-            species_to_gene_symbol_to_embedding[species_name][gene_symbol.lower()]
-            for gene_symbol in adata.var_names
-        ])
-        for species_name in species_names
-    }
+    filtered_adata = adata[:, adata.var_names.isin(genes_to_use)]
+    filtered = adata.var_names.shape[0] - filtered_adata.var_names.shape[0]
+    logger.info(f'Filtered out {filtered} genes to a total of {filtered_adata.var_names.shape[0]} genes with embeddings.')
 
-    return adata, species_to_gene_embeddings, species_to_gene_symbol_to_embedding
+    # Load gene symbols for desired species for later use with indexes
+    species_to_all_gene_symbols = {
+        species: [
+            gene_symbol.lower()
+            for gene_symbol, _ in torch.load(species_to_gene_embedding_path[species]).items()
+        ]
+        for species in species_names
+    }
+    return filtered_adata, species_to_all_gene_symbols
