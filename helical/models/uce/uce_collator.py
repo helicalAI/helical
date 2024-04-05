eb31011470bd5c28d329a19e5526ec1cc9b8e034
Benoit Putzeys <benoit@helical-ai.com>
2024-04-05 16:25:49 +0200

    Cleanup uce_utils
    - Move classes to separate folders
    - Cleanup imports

---
+++ helical/models/uce/uce_collator.py
@@ -0,0 +1,28 @@
+import torch
+
+class UCECollator(object):
+    def __init__(self, config):
+        self.pad_length = config['pad_length']
+
+
+    def __call__(self, batch):
+        batch_size = len(batch)
+        batch_sentences = torch.zeros((batch_size, self.pad_length))
+        mask = torch.zeros((batch_size, self.pad_length))
+        cell_sentences = torch.zeros((batch_size, self.pad_length))
+
+        idxs = torch.zeros(batch_size)
+
+        i = 0
+        max_len = 0
+        for bs, msk, idx, seq_len, cs in batch:
+            batch_sentences[i, :] = bs
+            cell_sentences[i, :] = cs
+            max_len = max(max_len, seq_len)
+            mask[i, :] = msk
+            idxs[i] = idx
+
+            i += 1
+
+        return batch_sentences[:, :max_len] , mask[:, :max_len], idxs, cell_sentences
+    
--- helical/models/uce/uce_utils.py
+++ helical/models/uce/uce_utils.py
@@ -1,172 +1,20 @@
 import scanpy as sc
-from torch.utils.data import Dataset, DataLoader
 import torch
-import numpy as np
+from torch.utils.data import DataLoader
 import pickle
 import pandas as pd
 import numpy as np
-import torch
 from tqdm import tqdm
-from torch import nn
 import scipy
 from pathlib import Path
 from typing import Dict, Tuple
-from scanpy import AnnData
+import logging
 
 from helical.models.uce.gene_embeddings import load_gene_embeddings_adata
 from helical.models.uce.uce_model import TransformerModel
-import logging
+from helical.models.uce.uce_dataset import UCEDataset
+
 logger = logging.getLogger(__name__)
-class UCECollator(object):
-    def __init__(self, config):
-        self.pad_length = config['pad_length']
-
-
-    def __call__(self, batch):
-        batch_size = len(batch)
-        batch_sentences = torch.zeros((batch_size, self.pad_length))
-        mask = torch.zeros((batch_size, self.pad_length))
-        cell_sentences = torch.zeros((batch_size, self.pad_length))
-
-        idxs = torch.zeros(batch_size)
-
-        i = 0
-        max_len = 0
-        for bs, msk, idx, seq_len, cs in batch:
-            batch_sentences[i, :] = bs
-            cell_sentences[i, :] = cs
-            max_len = max(max_len, seq_len)
-            mask[i, :] = msk
-            idxs[i] = idx
-
-            i += 1
-
-        return batch_sentences[:, :max_len] , mask[:, :max_len], idxs, cell_sentences
-    
-class UCEDataset(Dataset):
-    def __init__(self, 
-                 sorted_dataset_names,
-                 shapes_dict,
-                 model_config, 
-                 dataset_to_protein_embeddings,
-                 datasets_to_chroms,
-                 datasets_to_starts,
-                 expression_counts_path) -> None:
-        super(UCEDataset, self).__init__()
-        # self.xs = {}
-        self.num_cells = {}
-        self.num_genes = {}
-        self.shapes_dict = shapes_dict
-        self.config = model_config
-        self.collator_fn = UCECollator(model_config)
-
-        self.total_num_cells = 0
-        for name in sorted_dataset_names:
-            num_cells, num_genes = self.shapes_dict[name]
-            # self.xs[name] = X
-            self.num_cells[name] = num_cells
-            self.num_genes[name] = num_genes
-
-            self.total_num_cells += num_cells
-
-        self.datasets = sorted_dataset_names
-
-        # TODO: preferably not hard-coded here
-        self.dataset_to_protein_embeddings = dataset_to_protein_embeddings
-        self.dataset_to_chroms = datasets_to_chroms
-        self.dataset_to_starts = datasets_to_starts
-        self.npzs_dir = expression_counts_path
-
-    def __getitem__(self, idx):
-        if isinstance(idx, int):
-            for dataset in sorted(self.datasets):
-                if idx < self.num_cells[dataset]:
-                    cts = np.memmap(self.npzs_dir + f"{dataset}_counts.npz", dtype='int64', mode='r', shape=self.shapes_dict[dataset])
-                    counts = cts[idx]
-                    counts = torch.tensor(counts).unsqueeze(0)
-                    weights = torch.log1p(counts)
-                    weights = (weights / torch.sum(weights))
-                    batch_sentences, mask, seq_len, cell_sentences = \
-                        self.sample_cell_sentences(counts, weights)
-                    return batch_sentences, mask, idx, seq_len, cell_sentences
-                else:
-                    idx -= self.num_cells[dataset]
-            raise IndexError
-        else:
-            raise NotImplementedError
-
-    def __len__(self) -> int:
-        return self.total_num_cells
-
-    def get_dim(self) -> Dict[str, int]:
-        return self.num_genes
-    
-    def sample_cell_sentences(self,counts, batch_weights):
-
-        dataset_idxs = self.dataset_to_protein_embeddings # get the dataset specific protein embedding idxs
-        cell_sentences = torch.zeros((counts.shape[0], self.config["pad_length"])) # init the cell representation as 0s
-        mask = torch.zeros((counts.shape[0], self.config["pad_length"])) # start of masking the whole sequence
-        chroms = self.dataset_to_chroms # get the dataset specific chroms for each gene
-        starts = self.dataset_to_starts # get the dataset specific genomic start locations for each gene
-
-        longest_seq_len = 0 # we need to keep track of this so we can subset the batch at the end
-        for c, cell in enumerate(counts):
-            weights = batch_weights[c].numpy()
-            weights = weights / sum(weights)  # RE NORM after mask
-            
-            # randomly choose the genes that will make up the sample, weighted by expression, with replacement
-            choice_idx = np.random.choice(np.arange(len(weights)),
-                                        size=self.config["sample_size"], p=weights,
-                                        replace=True)
-
-            choosen_chrom = chroms[choice_idx] # get the sampled genes chromosomes
-            # order the genes by chromosome
-            chrom_sort = np.argsort(choosen_chrom)  
-            choice_idx = choice_idx[chrom_sort]
-
-            # sort the genes by start
-            new_chrom = chroms[choice_idx]
-            choosen_starts = starts[choice_idx]
-
-            ordered_choice_idx = np.full((self.config["pad_length"]),
-                                        self.config["cls_token_idx"])  # start with cls
-            # i= 0 first token is CLS
-            i = 1  # continue on to the rest of the sequence with left bracket being assumed.
-            # Shuffle the chroms now, there's no natural order to chromosomes
-            uq_chroms = np.unique(new_chrom)
-            np.random.shuffle(uq_chroms) # shuffle
-            
-            # This loop is actually just over one cell
-            for chrom in uq_chroms:
-                # Open Chrom token
-                ordered_choice_idx[i] = int(chrom) + self.config["CHROM_TOKEN_OFFSET"] # token of this chromosome # i = 1 next token is a chrom open
-                i += 1
-                # now sort the genes by start order within the chroms
-                loc = np.where(new_chrom == chrom)[0]
-                sort_by_start = np.argsort(
-                    choosen_starts[loc])  # start locations for this chromsome
-
-                to_add = choice_idx[loc[sort_by_start]]
-                ordered_choice_idx[i:(i + len(to_add))] = dataset_idxs[to_add]
-                i += len(to_add)
-                ordered_choice_idx[i] = self.config["chrom_token_right_idx"] # add the chrom sep again
-                i += 1  # add the closing token again
-
-            longest_seq_len = max(longest_seq_len, i)
-            remainder_len = (self.config["pad_length"] - i)
-
-            cell_mask = torch.concat((torch.ones(i),
-                                    # pay attention to all of these tokens, ignore the rest!
-                                    torch.zeros(remainder_len)))
-
-            mask[c, :] = cell_mask
-
-            ordered_choice_idx[i:] = self.config["pad_token_idx"] # the remainder of the sequence
-            cell_sentences[c, :] = torch.from_numpy(ordered_choice_idx)
-            
-        cell_sentences_pe = cell_sentences.long() # token indices
-        
-        return cell_sentences_pe, mask, longest_seq_len, cell_sentences
 
 def process_data(anndata, model_config, files_config, species='human', filter_genes=False, accelerator=None):
         if filter_genes:
@@ -226,7 +74,7 @@ def process_data(anndata, model_config, files_config, species='human', filter_ge
         #     # pbar = tqdm(dataloader)
         return dataloader
 
-def get_positions(species_chrom_csv_path: Path, species: str, adata: AnnData) -> Tuple[pd.Series, np.array]:
+def get_positions(species_chrom_csv_path: Path, species: str, adata: sc.AnnData) -> Tuple[pd.Series, np.array]:
     """
     Get the chromosomes to which the genes in adata belong (encoded with cat.codes) and the start positions of the genes
     
@@ -271,7 +119,7 @@ def get_ESM2_embeddings(files_config: Dict[str, str]) -> torch.Tensor:
 
     return all_pe
 
-def get_protein_embeddings_idxs(offset_pkl_path: str, species: str, species_to_all_gene_symbols: Dict[str, list[str]], adata: AnnData)-> torch.tensor:
+def get_protein_embeddings_idxs(offset_pkl_path: str, species: str, species_to_all_gene_symbols: Dict[str, list[str]], adata: sc.AnnData)-> torch.tensor:
     """
     For a given species, look up the offset. Find the index per gene from the list of all available genes.
     Only use the indexes of the genes for which there are embeddings (in adata).
@@ -337,7 +185,7 @@ def load_model(model_config: Dict[str, str], all_pe: torch.Tensor) -> Transforme
     # empty_pe = torch.zeros(50000, 5120)
     empty_pe = torch.zeros(145469, 5120)
     empty_pe.requires_grad = False
-    model.pe_embedding = nn.Embedding.from_pretrained(empty_pe)
+    model.pe_embedding = torch.nn.Embedding.from_pretrained(empty_pe)
     model.load_state_dict(torch.load(model_config['model_loc'], map_location="cpu"), strict=True)
 
     # TODO: Why load the protein embeddings from the `all_tokens.torch` file, pass it to this function but never use it?
@@ -348,7 +196,7 @@ def load_model(model_config: Dict[str, str], all_pe: torch.Tensor) -> Transforme
     # We avoid doing that just in case the random seeds are different across different versions. 
     if all_pe.shape[0] != 145469: 
         all_pe.requires_grad = False
-        model.pe_embedding = nn.Embedding.from_pretrained(all_pe)
+        model.pe_embedding = torch.nn.Embedding.from_pretrained(all_pe)
     
     return model
 
@@ -364,7 +212,7 @@ def get_gene_embeddings(model, dataloader, accelerator, model_config=None):
                 batch_sentences = model.module.pe_embedding(batch_sentences.long())
             else:
                 batch_sentences = model.pe_embedding(batch_sentences.long())
-            batch_sentences = nn.functional.normalize(batch_sentences,
+            batch_sentences = torch.nn.functional.normalize(batch_sentences,
                                                       dim=2)  # Normalize token outputs now
             _, embedding = model.forward(batch_sentences, mask=mask)
             # Fix for duplicates in last batch
