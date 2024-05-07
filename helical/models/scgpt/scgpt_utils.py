2efb9098915733aa578c3490c1aa6b85e9b72d0d
maxiallard <maxime.allard@yahoo.de>
2024-05-07 13:53:30 +0200

    added simplified scgpt

---
+++ helical/models/scgpt/scgpt_utils.py
@@ -0,0 +1,157 @@
+import json
+import os
+from pathlib import Path
+from typing import Optional, Union
+from os import PathLike
+
+import numpy as np
+import scanpy as sc
+import torch
+from anndata import AnnData
+from torch.utils.data import DataLoader, SequentialSampler
+from tqdm import tqdm
+
+
+from scgpt.data_collator import DataCollator
+from scgpt.model import TransformerModel
+from scgpt.tokenizer import GeneVocab
+from scgpt.utils import load_pretrained
+
+from scgpt.tasks.cell_emb import get_batch_cell_embeddings
+
+
+
+def load_model(model_dir,model_configs,device='cpu',use_fast_transformer=False):
+
+    # LOAD MODEL
+    model_dir = Path(model_dir)
+    vocab_file = model_dir / "vocab.json"
+    model_file = model_dir / "best_model.pt"
+    pad_token = "<pad>"
+    special_tokens = [pad_token, "<cls>", "<eoc>"]
+
+    # vocabulary
+    vocab = GeneVocab.from_file(vocab_file)
+    for s in special_tokens:
+        if s not in vocab:
+            vocab.append_token(s)
+
+    # Binning will be applied after tokenization. A possible way to do is to use the unified way of binning in the data collator.
+
+    vocab.set_default_index(vocab["<pad>"])
+
+    model = TransformerModel(
+        ntoken=len(vocab),
+        d_model=model_configs["embsize"],
+        nhead=model_configs["nheads"],
+        d_hid=model_configs["d_hid"],
+        nlayers=model_configs["nlayers"],
+        nlayers_cls=model_configs["n_layers_cls"],
+        n_cls=1,
+        vocab=vocab,
+        dropout=model_configs["dropout"],
+        pad_token=model_configs["pad_token"],
+        pad_value=model_configs["pad_value"],
+        do_mvc=True,
+        do_dab=False,
+        use_batch_labels=False,
+        domain_spec_batchnorm=False,
+        explicit_zero_prob=False,
+        use_fast_transformer=use_fast_transformer,
+        fast_transformer_backend="flash",
+        pre_norm=False,
+    )
+
+    load_pretrained(model, torch.load(model_file, map_location=device), verbose=False)
+    model.to(device)
+    model.eval()
+    return model,vocab
+
+
+
+
+def get_embedding(
+    adata_or_file: Union[AnnData, PathLike],
+    model_configs: dict,
+    model: TransformerModel,
+    vocab: GeneVocab,
+    gene_col: str = "feature_name",
+    max_length=1200,
+    batch_size=64,
+    obs_to_save: Optional[list] = None,
+    device: Union[str, torch.device] = "cuda",
+    use_fast_transformer: bool = True,
+    return_new_adata: bool = False,
+) -> AnnData:
+    """
+    Preprocess anndata and embed the data using the model.
+
+    Args:
+        adata_or_file (Union[AnnData, PathLike]): The AnnData object or the path to the
+            AnnData object.
+        model_configs (dict): A dictionary with the model configurations.
+        gene_col (str): The column in adata.var that contains the gene names.
+        max_length (int): The maximum length of the input sequence. Defaults to 1200.
+        batch_size (int): The batch size for inference. Defaults to 64.
+        obs_to_save (Optional[list]): The list of obs columns to save in the output adata.
+            Useful for retaining meta data to output. Defaults to None.
+        device (Union[str, torch.device]): The device to use. Defaults to "cuda".
+        use_fast_transformer (bool): Whether to use flash-attn. Defaults to True.
+        return_new_adata (bool): Whether to return a new AnnData object. If False, will
+            add the cell embeddings to a new :attr:`adata.obsm` with key "X_scGPT".
+
+    Returns:
+        AnnData: The AnnData object with the cell embeddings.
+    """
+    if isinstance(adata_or_file, AnnData):
+        adata = adata_or_file
+    else:
+        adata = sc.read_h5ad(adata_or_file)
+
+    if isinstance(obs_to_save, str):
+        assert obs_to_save in adata.obs, f"obs_to_save {obs_to_save} not in adata.obs"
+        obs_to_save = [obs_to_save]
+
+    # verify gene col
+    if gene_col == "index":
+        adata.var["index"] = adata.var.index
+    else:
+        assert gene_col in adata.var
+
+    if device == "cuda":
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        if not torch.cuda.is_available():
+            print("WARNING: CUDA is not available. Using CPU instead.")
+
+    adata.var["id_in_vocab"] = [
+        vocab[gene] if gene in vocab else -1 for gene in adata.var[gene_col]
+    ]
+    gene_ids_in_vocab = np.array(adata.var["id_in_vocab"])
+    adata = adata[:, adata.var["id_in_vocab"] >= 0]
+
+    # Binning will be applied after tokenization. A possible way to do is to use the unified way of binning in the data collator.
+
+    vocab.set_default_index(vocab["<pad>"])
+    genes = adata.var[gene_col].tolist()
+    gene_ids = np.array(vocab(genes), dtype=int)
+
+    # get cell embeddings
+    cell_embeddings = get_batch_cell_embeddings(
+        adata,
+        cell_embedding_mode="cls",
+        model=model,
+        vocab=vocab,
+        max_length=max_length,
+        batch_size=batch_size,
+        model_configs=model_configs,
+        gene_ids=gene_ids,
+        use_batch_labels=False,
+    )
+
+    if return_new_adata:
+        obs_df = adata.obs[obs_to_save] if obs_to_save is not None else None
+        return sc.AnnData(X=cell_embeddings, obs=obs_df, dtype="float32")
+
+    # adata.obsm["X_scGPT"] = cell_embeddings
+
+    return cell_embeddings
