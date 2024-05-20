f8f4c64ff89baaf43462e99b7a959387c1ff9fa4
Benoit Putzeys <benoit@helical-ai.com>
2024-05-20 16:17:36 +0200

    Add downloading Hyena model

--- ci/download_all.py
+++ ci/download_all.py
@@ -11,8 +11,7 @@ def main():
     downloader.download_via_name("uce/species_offsets.pkl")
     downloader.download_via_name("uce/protein_embeddings/Homo_sapiens.GRCh38.gene_symbol_to_embedding_ESM2.pt")
     downloader.download_via_name("uce/protein_embeddings/Macaca_fascicularis.Macaca_fascicularis_6.0.gene_symbol_to_embedding_ESM2.pt")
-    downloader.download_via_link(Path("./10k_pbmcs_proc.h5ad"), "https://helicalpackage.blob.core.windows.net/helicalpackage/data/10k_pbmcs_proc.h5ad")
-
+    
     downloader.download_via_name("scgpt/scGPT_CP/vocab.json")
     downloader.download_via_name("scgpt/scGPT_CP/best_model.pt")
 
@@ -23,6 +22,10 @@ def main():
     downloader.download_via_name("geneformer/geneformer-12L-30M/pytorch_model.bin")
     downloader.download_via_name("geneformer/geneformer-12L-30M/training_args.bin")
 
+    downloader.download_via_name("hyena_dna/hyenadna-tiny-1k-seqlen.ckpt")
+    downloader.download_via_name("hyena_dna/hyenadna-tiny-1k-seqlen-d256.ckpt")
+
+    downloader.download_via_link(Path("./10k_pbmcs_proc.h5ad"), "https://helicalpackage.blob.core.windows.net/helicalpackage/data/10k_pbmcs_proc.h5ad")
     return True
 
 if __name__ == "__main__":
