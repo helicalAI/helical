dc275a96f2a542ad36701ae869e15cd8971fc7eb
Benoit Putzeys <benoit@helical-ai.com>
2024-06-21 12:36:31 +0200

    Leave "ensemble_id" as default gene_column_name to emphasise that it geneformer uses it always.

--- examples/run_models/run_geneformer.py
+++ examples/run_models/run_geneformer.py
@@ -5,7 +5,7 @@ geneformer_config = GeneformerConfig(batch_size = 10)
 geneformer = Geneformer(configurer = geneformer_config)
 
 ann_data = ad.read_h5ad("./10k_pbmcs_proc.h5ad")
-dataset = geneformer.process_data(ann_data[:10])
+dataset = geneformer.process_data(ann_data[:10], "gene_symbols")
 embeddings = geneformer.get_embeddings(dataset)
 
 print(embeddings.shape)
