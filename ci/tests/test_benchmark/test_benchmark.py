04f42ec179aa2d008d08a4089f2f79ef8a9694ef
Benoit Putzeys <benoit@helical-ai.com>
2024-07-03 17:20:35 +0200

    Fix pytest to remove Benchmark class

--- ci/tests/test_benchmark/test_benchmark.py
+++ ci/tests/test_benchmark/test_benchmark.py
@@ -1,4 +1,4 @@
-from helical.benchmark.benchmark import Benchmark
+from helical.benchmark.benchmark import evaluate_classification
 from helical.models.classification.classifier import Classifier
 from helical.models.classification.neural_network import NeuralNetwork
 from helical.models.scgpt.model import scGPT
@@ -23,7 +23,6 @@ def test_evaluate_classification(mocker):
     head.predict.return_value = np.array(['type1', 'type2'] * (data.shape[0] // 2))
 
     scgpt_nn_c = Classifier().train_classifier_head(data, scgpt, head, gene_col_name = "gene_symbols", labels_column_name = "cell_type")
-    bench = Benchmark()
-    evaluations = bench.evaluate_classification([scgpt_nn_c, scgpt_nn_c], data, "cell_type")
+    evaluations = evaluate_classification([scgpt_nn_c, scgpt_nn_c], data, "cell_type")
     assert evaluations == {'scGPT with NeuralNetwork': {'Accuracy': 1.0, 'Precision': 1.0, 'F1': 1.0, 'Recall': 1.0}, 
                            'scGPT with NeuralNetwork': {'Accuracy': 1.0, 'Precision': 1.0, 'F1': 1.0, 'Recall': 1.0}}
