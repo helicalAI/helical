e31fb3e9b3d6c68563018bc0ff419e312bbd4704
Benoit Putzeys <benoit@helical-ai.com>
2024-07-03 15:32:06 +0200

    Include scanorama for batch integration comparison

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'einops==0.8.0',
         'omegaconf==2.3.0',
         'hydra-core==1.3.2',
-        'tensorflow==2.13.0rc1'
+        'tensorflow==2.13.0rc1',
+        'scanorama==1.7.4',
     ],  
 )
