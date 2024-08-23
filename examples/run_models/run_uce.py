1477801476360a72c4d43c1faaebc1c2ff746db9
Benoit Putzeys <benoit@helical-ai.com>
2024-08-23 15:37:13 +0200

    Iterate trhough the data in batches to reduce
    foodprint that is saved to the npz file

--- examples/run_models/run_uce.py
+++ examples/run_models/run_uce.py
@@ -8,10 +8,23 @@ def run(cfg: DictConfig):
     configurer=UCEConfig(**cfg)
     uce = UCE(configurer=configurer)
     ann_data = ad.read_h5ad("./10k_pbmcs_proc.h5ad")
-    dataset = uce.process_data(ann_data[:10])
-    embeddings = uce.get_embeddings(dataset)
+    batch_size = 1000
 
-    print(embeddings.shape)
+    # Initialize a list to store embeddings from each batch
+    all_embeddings = []
+
+    # Iterate over the data in batches
+    for start in range(0, ann_data.shape[0], batch_size):
+        end = min(start + batch_size, ann_data.shape[0])
+        ann_data_batch = ann_data[start:end].copy()
+        dataset_batch = uce.process_data(ann_data_batch)
+        embeddings_batch = uce.get_embeddings(dataset_batch)
+        all_embeddings.append(embeddings_batch)
+        
+        # Optionally, you can print progress
+        print(f"Processed batch {start} to {end}")
+
+    print(all_embeddings.shape)
 
 if __name__ == "__main__":
     run()
