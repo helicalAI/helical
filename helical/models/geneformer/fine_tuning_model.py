b8a80634849e51b282c80d3c88b6604d72c04688
Matthew Wood <matthew@helical-ai.com>
2024-09-20 15:45:00 +0200

    Update docstrings

--- helical/models/geneformer/fine_tuning_model.py
+++ helical/models/geneformer/fine_tuning_model.py
@@ -23,7 +23,7 @@ class GeneformerFineTuningModel(HelicalBaseFineTuningModel):
     geneformer_model : Geneformer
         The initialised Geneformer model to fine-tune.
     fine_tuning_head : Literal["classification"] | HelicalBaseFineTuningHead
-        The fine-tuning head that is appended to the model. This can either be a string specifying the task or a custom fine-tuning head inheriting from HelicalBaseFineTuningHead.
+        The fine-tuning head that is appended to the model. This can either be a string (options available: "classification") specifying the task or a custom fine-tuning head inheriting from HelicalBaseFineTuningHead.
     output_size : Optional[int]
         The output size of the fine-tuning model. This is required if the fine_tuning_head is a string specified task. For a classification task this is number of unique classes.
 
