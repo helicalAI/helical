ff231738469516cf4b79400b533f0b5b8d11e82b
Benoit Putzeys <benoit@helical-ai.com>
2024-09-30 08:13:42 +0200

    Adjust docstring and type hints. Include error handling if wrong number of genes is provided.

--- helical/utils/dataset_to_anndata.py
+++ helical/utils/dataset_to_anndata.py
@@ -2,23 +2,53 @@ import pandas as pd
 import anndata as ad
 import numpy as np
 from scipy.sparse import lil_matrix
+from datasets import Dataset
+import logging
 
-def get_anndata_from_hf_dataset(dataset):
+LOGGER = logging.getLogger(__name__)
+
+def get_anndata_from_hf_dataset(dataset: Dataset) -> ad.AnnData:
     """
     Convert a HuggingFace dataset to an AnnData object
+    It assumes that the dataset has the following columns:
+    - raw_counts
+    - rows TODO difference between these two??
+    - Any other column that will be used as obs
+
+    The id of the raw_counts are the gene names as a string separated by commas.
 
     Parameters
     ----------
     dataset : Dataset
         A HuggingFace dataset object
+
+    Returns
+    -------
+    AnnData
+        An AnnData object containing the data from the input dataset
     """
-    observation_columns = [obs for obs in list(dataset.features.keys()) if not obs == 'raw_counts' and not obs == "rows"]
-    obs_data = pd.DataFrame(dataset.select_columns(observation_columns).data.to_pandas(),columns=observation_columns)
+    # obs
+    observation_names = [obs for obs in list(dataset.features.keys()) if not obs == 'raw_counts' and not obs == "rows"]
+    obs_data = pd.DataFrame(dataset.select_columns(observation_names).data.to_pandas(),columns=observation_names)
+    
+    # raw counts
     lil = lil_matrix((len(dataset),dataset[0]['size']))
     lil.data = np.array(dataset['raw_counts'],dtype="object")
     lil.rows = np.array(dataset['rows'],dtype="object")
+    
+    # create AnnData object
     ann_data = ad.AnnData(lil.tocsr(),obs=obs_data)
-    ann_data.var_names = dataset.features['raw_counts'].id.split(",")
+
+    # add gene names
+    var_names = dataset.features['raw_counts'].id.split(",")
+    if len(var_names) != ann_data.shape[1]:
+        message = f"Number of gene names ({len(var_names)}) does not match the number of genes ({ann_data.shape[1]})"
+        LOGGER.error(message)
+        raise ValueError(message)
+
+    ann_data.var_names = var_names
+    
+    # TODO: necessary?
     ann_data.var['gene_name'] = ann_data.var_names.str.upper()
 
     return ann_data
