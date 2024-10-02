a3fcf5e8a9c1913d4e71f2821c69220fefbd6c9c
Matthew Wood <matthew@helical-ai.com>
2024-10-02 09:42:28 +0200

    Add regression head back

--- helical/models/fine_tune/fine_tuning_heads.py
+++ helical/models/fine_tune/fine_tuning_heads.py
@@ -63,3 +63,51 @@ class ClassificationHead(HelicalBaseFineTuningHead):
             The dimension size of the input tensor.
         """
         self.linear = torch.nn.Linear(dim_size, self.output_size)
+
+class RegressionHead(HelicalBaseFineTuningHead):
+    """Regression Head for fine-tuning a Helical foundation model.
+
+    Parameters
+    ----------
+    num_classes : int
+        The number of outputs to predict.
+    dropout : float, optional, default=0.02
+        The dropout rate to apply to the input tensor before the linear layer.
+        
+    Methods
+    -------
+    forward(inputs: torch.Tensor) -> torch.Tensor
+        The forward method of the regression head.
+
+    """
+    def __init__(self, num_outputs: int, dropout: float = 0.02):
+        super().__init__()
+        self.output_size = num_outputs
+        self.dropout = torch.nn.Dropout(p=dropout)
+    
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        """Forward method of the classification head.
+        
+        Parameters
+        ----------
+        inputs : torch.Tensor
+            The input tensor to the classification head.
+
+        Returns
+        -------
+        torch.Tensor
+            The output tensor of the classification head.
+        """
+        drop = self.dropout(inputs)
+        output = self.linear(drop)
+        return output
+
+    def set_dim_size(self, dim_size: int) -> None:
+        """Set the dimension size of the input tensor.
+        
+        Parameters
+        ----------
+        dim_size : int
+            The dimension size of the input tensor.
+        """
+        self.linear = torch.nn.Linear(dim_size, self.output_size)
