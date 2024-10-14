e5bcdd717df4aa9b7a220b29e44aab7a8f8a50a0
Benoit Putzeys <benoit@helical-ai.com>
2024-10-14 18:42:01 +0200

    Open the file only after a successful response has been received. Otherwise  file will be opened even if the request fails, resulting in an empty file.

--- helical/utils/downloader.py
+++ helical/utils/downloader.py
@@ -88,20 +88,20 @@ class Downloader(Logger):
 
         else:
             LOGGER.info(f"Starting to download: '{link}'")
+            response = requests.get(link, stream=True)
+
+            if response.status_code != 200:
+                message = f"Failed downloading file from '{link}' with status code: {response.status_code}"
+                LOGGER.error(message)
+                raise Exception(message)
+            
+            total_length = response.headers.get('content-length')
+
+            # Resetting for visualization
+            self.data_length = 0
+            self.total_length = int(total_length)
+
             with open(output, "wb") as f:
-                response = requests.get(link, stream=True)
-
-                if response.status_code != 200:
-                    message = f"Failed downloading file from '{link}' with status code: {response.status_code}"
-                    LOGGER.error(message)
-                    raise Exception
-                
-                total_length = response.headers.get('content-length')
-
-                # Resetting for visualization
-                self.data_length = 0
-                self.total_length = int(total_length)
-
                 if total_length is None: # no content length header
                     f.write(response.content)
                 else:
