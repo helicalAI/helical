ad2f6532ac57e51ebae3cefd24946a31bd580e4f
Benoit Putzeys <benoit@helical-ai.com>
2024-11-04 16:36:44 +0100

    Remove unused version file

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 from setuptools import setup, find_packages
-# from helical.version import __version__
 
 setup(
     name='helical',
-    # version=__version__,
-
     url='https://github.com/helicalAI/helical-package.git',
     author='Benoit Putzeys, Maxime Allard',
     author_email='benoit@helical-ai.com, maxime@helical-ai.com',
