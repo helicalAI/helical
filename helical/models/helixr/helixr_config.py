9232ee001275c723bde8d7aa63b1355b3e8f4e2d
Matthew Wood <matthew@helical-ai.com>
2024-11-05 19:23:04 +0100

    Add docstrings to config class for HelixR

--- helical/models/helixr/helixr_config.py
+++ helical/models/helixr/helixr_config.py
@@ -4,6 +4,19 @@ from helical.constants.paths import CACHE_DIR_HELICAL
 from typing import Literal
 
 class HelixRConfig:
+    """HelixRConfig class to store the configuration of the HelixR model.
+    
+    Parameters
+    ----------
+    model_name : Literal["helixR-8L"], optional, default = "helixR-8L"
+        The name of the model.
+    batch_size : int, optional, default = 24
+        The batch size
+    device : Literal["cpu", "cuda"], optional, default = "cpu"
+        The device to use. Either use "cuda" or "cpu".
+    nproc: int, optional, default = 1
+        Number of processes to use for data processing.
+    """
     def __init__(self, 
             model_name: Literal["helixR-8L"] = "helixR-8L",
             batch_size: int = 24,
