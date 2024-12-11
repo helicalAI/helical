2657c910367b3cc154f45ae775d487b7b980b8fe
Benoit Putzeys <benoit@helical-ai.com>
2024-12-11 17:04:40 +0100

    Remove unused utils folder except for
    load_pretrained which is now in scgpt_utils.py

--- helical/models/scgpt/scgpt_utils.py
+++ helical/models/scgpt/scgpt_utils.py
@@ -1,8 +1,69 @@
 import torch
 from .model_dir import TransformerModel
-from .utils import load_pretrained
 from helical.models.scgpt.scgpt_config import scGPTConfig
 import json
+from typing import List, Mapping, Optional
+import torch
+
+def load_pretrained(
+    model: torch.nn.Module,
+    pretrained_params: Mapping[str, torch.Tensor],
+    strict: bool = False,
+    prefix: Optional[List[str]] = None,
+    verbose: bool = True,
+) -> torch.nn.Module:
+    """
+    Load pretrained weights to the model.
+
+    Args:
+        model (torch.nn.Module): The model to load weights to.
+        pretrained_params (Mapping[str, torch.Tensor]): The pretrained parameters.
+        strict (bool): Whether to strictly enforce that the keys in :attr:`pretrained_params`
+            match the keys returned by this module's :meth:`Module.state_dict`. Default to False.
+        prefix (List[str]): The list of prefix strings to match with the keys in
+            :attr:`pretrained_params`. The matched keys will be loaded. Default to None.
+
+    Returns:
+        torch.nn.Module: The model with pretrained weights.
+    """
+
+    use_flash_attn = getattr(model, "use_fast_transformer", True)
+    if not use_flash_attn:
+        pretrained_params = {
+            k.replace("Wqkv.", "in_proj_"): v for k, v in pretrained_params.items()
+        }
+
+    if prefix is not None and len(prefix) > 0:
+        if isinstance(prefix, str):
+            prefix = [prefix]
+        pretrained_params = {
+            k: v
+            for k, v in pretrained_params.items()
+            if any(k.startswith(p) for p in prefix)
+        }
+
+    model_dict = model.state_dict()
+    if strict:
+        if verbose:
+            for k, v in pretrained_params.items():
+                logger.info(f"Loading parameter {k} with shape {v.shape}")
+        model_dict.update(pretrained_params)
+        model.load_state_dict(model_dict)
+    else:
+        if verbose:
+            for k, v in pretrained_params.items():
+                if k in model_dict and v.shape == model_dict[k].shape:
+                    logger.info(f"Loading parameter {k} with shape {v.shape}")
+        pretrained_params = {
+            k: v
+            for k, v in pretrained_params.items()
+            if k in model_dict and v.shape == model_dict[k].shape
+        }
+        model_dict.update(pretrained_params)
+        model.load_state_dict(model_dict)
+
+    return model
+
 
 def load_model(model_configs: scGPTConfig):
 
