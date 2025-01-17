7d1f7a4148360bec50d0195d50fddba94a5bf655
bputzeys <benoit@helical-ai.com>
2025-01-17 14:37:42 +0100

    Check if downloaded file has correct hash, calculated from first and last chunk of the data with sha256

--- ci/download_all.py
+++ ci/download_all.py
@@ -57,7 +57,13 @@ def main():
     downloader.download_via_name("uce/species_offsets.pkl")
     downloader.download_via_name("uce/protein_embeddings/Homo_sapiens.GRCh38.gene_symbol_to_embedding_ESM2.pt")
     downloader.download_via_name("uce/protein_embeddings/Macaca_fascicularis.Macaca_fascicularis_6.0.gene_symbol_to_embedding_ESM2.pt")
-    
+    downloader.download_via_name("uce/protein_embeddings/Danio_rerio.GRCz11.gene_symbol_to_embedding_ESM2.pt")
+    downloader.download_via_name("uce/protein_embeddings/Macaca_mulatta.Mmul_10.gene_symbol_to_embedding_ESM2.pt")
+    downloader.download_via_name("uce/protein_embeddings/Microcebus_murinus.Mmur_3.0.gene_symbol_to_embedding_ESM2.pt")
+    downloader.download_via_name("uce/protein_embeddings/Mus_musculus.GRCm39.gene_symbol_to_embedding_ESM2.pt")
+    downloader.download_via_name("uce/protein_embeddings/Sus_scrofa.Sscrofa11.1.gene_symbol_to_embedding_ESM2.pt")
+    downloader.download_via_name("uce/protein_embeddings/Xenopus_tropicalis.Xenopus_tropicalis_v9.1.gene_symbol_to_embedding_ESM2.pt")
+
     downloader.download_via_name("scgpt/scGPT_CP/vocab.json")
     downloader.download_via_name("scgpt/scGPT_CP/best_model.pt")
 
@@ -65,6 +71,24 @@ def main():
 
     downloader.download_via_name("hyena_dna/hyenadna-tiny-1k-seqlen.ckpt")
     downloader.download_via_name("hyena_dna/hyenadna-tiny-1k-seqlen-d256.ckpt")
+    downloader.download_via_name("hyena_dna/hyenadna-small-32k-seqlen.ckpt")
+    downloader.download_via_name("hyena_dna/hyenadna-medium-450k-seqlen.ckpt")
+    downloader.download_via_name("hyena_dna/hyenadna-large-1m-seqlen.ckpt")
+
+    downloader.download_via_name('caduceus/caduceus-ph-16L-seqlen-131k-d256/model.safetensors')
+    downloader.download_via_name('caduceus/caduceus-ph-16L-seqlen-131k-d256/config.json')
+    downloader.download_via_name('caduceus/caduceus-ph-4L-seqlen-1k-d118/model.safetensors')
+    downloader.download_via_name('caduceus/caduceus-ph-4L-seqlen-1k-d118/config.json')
+    downloader.download_via_name('caduceus/caduceus-ph-4L-seqlen-1k-d256/model.safetensors')
+    downloader.download_via_name('caduceus/caduceus-ph-4L-seqlen-1k-d256/config.json')
+    downloader.download_via_name('caduceus/caduceus-ps-16L-seqlen-131k-d256/model.safetensors')
+    downloader.download_via_name('caduceus/caduceus-ps-16L-seqlen-131k-d256/config.json')
+    downloader.download_via_name('caduceus/caduceus-ps-4L-seqlen-1k-d118/model.safetensors')
+    downloader.download_via_name('caduceus/caduceus-ps-4L-seqlen-1k-d118/config.json')
+    downloader.download_via_name('caduceus/caduceus-ps-4L-seqlen-1k-d256/model.safetensors')
+    downloader.download_via_name('caduceus/caduceus-ps-4L-seqlen-1k-d256/config.json')
+
+    downloader.download_via_name('genept/genept_embeddings/genept_embeddings.json')
 
     downloader.download_via_link(Path("yolksac_human.h5ad"), "https://huggingface.co/datasets/helical-ai/yolksac_human/resolve/main/data/17_04_24_YolkSacRaw_F158_WE_annots.h5ad?download=true")
     return True
