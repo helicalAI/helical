ad246f75268ec7c2c0ee108441c3f2b0553e0515
Maxime <maxime@helical-ai.com>
2025-06-17 17:33:44 +0200

    added attention_maps for scgpt and geneformer

--- examples/run_models/run_scgpt.py
+++ examples/run_models/run_scgpt.py
@@ -12,16 +12,24 @@ def run(cfg: DictConfig):
     scgpt = scGPT(configurer=scgpt_config)
 
     # either load via huggingface
-    # hf_dataset = load_dataset("helical-ai/yolksac_human",split="train[:5%]", trust_remote_code=True, download_mode="reuse_cache_if_exists")
-    # ann_data = get_anndata_from_hf_dataset(hf_dataset)
+    hf_dataset = load_dataset(
+        "helical-ai/yolksac_human",
+        split="train[:5%]",
+        trust_remote_code=True,
+        download_mode="reuse_cache_if_exists",
+    )
+    ann_data = get_anndata_from_hf_dataset(hf_dataset)
 
     # or load directly
-    ann_data = ad.read_h5ad("./yolksac_human.h5ad")
+    # ann_data = ad.read_h5ad("./yolksac_human.h5ad")
 
     data = scgpt.process_data(ann_data[:10])
-    embeddings = scgpt.get_embeddings(data)
+    embeddings = scgpt.get_embeddings(data, output_attentions=False)
+    print(embeddings)
+    embeddings, attn_weights = scgpt.get_embeddings(data, output_attentions=True)
 
     print(embeddings)
+    print(attn_weights.shape)
 
 
 if __name__ == "__main__":
