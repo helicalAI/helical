6c923f2eaf6f242bccf910fc0c2b38b85f33db2d
Matthew Wood <matthew@helical-ai.com>
2025-07-28 13:17:54 +0200

    Addition of new geneformer v2 models Called them v3 because we already had new models added a while ago which we named v2. Hashes currently for one of the models

--- helical/models/geneformer/geneformer_config.py
+++ helical/models/geneformer/geneformer_config.py
@@ -9,7 +9,7 @@ class GeneformerConfig:
 
     Parameters
     ----------
-    model_name : Literal["gf-6L-30M-i2048", "gf-12L-30M-i2048", "gf-12L-95M-i4096", "gf-20L-95M-i4096", "gf-12L-95M-i4096-CLcancer"], optional, default="gf-12L-30M-i2048"
+    model_name : Literal["gf-6L-30M-i2048", "gf-12L-30M-i2048", "gf-12L-95M-i4096", "gf-20L-95M-i4096", "gf-12L-95M-i4096-CLcancer", "gf-12L-104M-i4096", "gf-12L-104M-i4096-CLcancer", "gf-18L-316M-i4096"], optional, default="gf-12L-30M-i2048"
         The name of the model.
     batch_size : int, optional, default = 24
         The batch size
@@ -21,8 +21,6 @@ class GeneformerConfig:
         For gene level embeddings, each gene token embedding is returned along with the corresponding ensembl ID.
     device : Literal["cpu", "cuda"], optional, default="cpu"
         The device to use. Either use "cuda" or "cpu".
-    accelerator : bool, optional, default=False
-        The accelerator configuration. By default same device as model.
     nproc: int, optional, default=1
         Number of processes to use for data processing.
     custom_attr_name_dict : dict, optional, default=None
@@ -45,12 +43,14 @@ class GeneformerConfig:
             "gf-12L-95M-i4096",
             "gf-20L-95M-i4096",
             "gf-12L-95M-i4096-CLcancer",
+            "gf-12L-104M-i4096",
+            "gf-12L-104M-i4096-CLcancer",
+            "gf-18L-316M-i4096",
         ] = "gf-12L-30M-i2048",
         batch_size: int = 24,
         emb_layer: int = -1,
         emb_mode: Literal["cls", "cell", "gene"] = "cell",
         device: Literal["cpu", "cuda"] = "cpu",
-        accelerator: Optional[bool] = False,
         nproc: int = 1,
         custom_attr_name_dict: Optional[dict] = None,
     ):
@@ -61,73 +61,100 @@ class GeneformerConfig:
                 "input_size": 4096,
                 "special_token": True,
                 "embsize": 512,
+                "model_version": "v2",
             },
             "gf-12L-95M-i4096-CLcancer": {
                 "input_size": 4096,
                 "special_token": True,
                 "embsize": 512,
+                "model_version": "v2",
             },
             "gf-20L-95M-i4096": {
                 "input_size": 4096,
                 "special_token": True,
                 "embsize": 896,
+                "model_version": "v2",
             },
             "gf-12L-30M-i2048": {
                 "input_size": 2048,
                 "special_token": False,
                 "embsize": 512,
+                "model_version": "v1",
             },
             "gf-6L-30M-i2048": {
                 "input_size": 2048,
                 "special_token": False,
                 "embsize": 256,
+                "model_version": "v1",
+            },
+            "gf-12L-104M-i4096": {
+                "input_size": 4096,
+                "special_token": True,
+                "embsize": 768,
+                "model_version": "v3",
+            },
+            "gf-12L-104M-i4096-CLcancer": {
+                "input_size": 4096,
+                "special_token": True,
+                "embsize": 768,
+                "model_version": "v3",
+            },
+            "gf-18L-316M-i4096": {
+                "input_size": 4096,
+                "special_token": True,
+                "embsize": 1152,
+                "model_version": "v3",
             },
         }
+
         if model_name not in self.model_map:
             raise ValueError(
                 f"Model name {model_name} not found in available models: {self.model_map.keys()}"
             )
 
-        model_version = "v2" if "95M" in model_name else "v1"
-
-        if model_version == "v1" and emb_mode == "cls":
+        if self.model_map[model_name]["model_version"] == "v1" and emb_mode == "cls":
             raise ValueError(
                 f"Verson 1 Geneformer models do not support the CLS embedding mode"
             )
 
         self.list_of_files_to_download = [
-            f"geneformer/{model_version}/gene_median_dictionary.pkl",
-            f"geneformer/{model_version}/token_dictionary.pkl",
-            f"geneformer/{model_version}/ensembl_mapping_dict.pkl",
-            f"geneformer/{model_version}/{model_name}/config.json",
-            f"geneformer/{model_version}/{model_name}/training_args.bin",
+            f"geneformer/{self.model_map[model_name]['model_version']}/gene_median_dictionary.pkl",
+            f"geneformer/{self.model_map[model_name]['model_version']}/token_dictionary.pkl",
+            f"geneformer/{self.model_map[model_name]['model_version']}/ensembl_mapping_dict.pkl",
+            f"geneformer/{self.model_map[model_name]['model_version']}/{model_name}/config.json",
+            f"geneformer/{self.model_map[model_name]['model_version']}/{model_name}/training_args.bin",
         ]
 
         # Add model weight files to download based on the model version (v1 or v2)
-        if model_version == "v2":
+        if (
+            self.model_map[model_name]["model_version"] == "v2"
+            or self.model_map[model_name]["model_version"] == "v3"
+        ):
             self.list_of_files_to_download.append(
-                f"geneformer/{model_version}/{model_name}/generation_config.json"
-            )
-            self.list_of_files_to_download.append(
-                f"geneformer/{model_version}/{model_name}/model.safetensors"
+                f"geneformer/{self.model_map[model_name]['model_version']}/{model_name}/model.safetensors"
             )
         else:
             self.list_of_files_to_download.append(
-                f"geneformer/{model_version}/{model_name}/pytorch_model.bin"
+                f"geneformer/{self.model_map[model_name]['model_version']}/{model_name}/pytorch_model.bin"
             )
 
         self.model_dir = Path(CACHE_DIR_HELICAL, "geneformer")
 
         self.files_config = {
             "model_files_dir": Path(
-                CACHE_DIR_HELICAL, "geneformer", model_version, model_name
+                CACHE_DIR_HELICAL,
+                "geneformer",
+                self.model_map[model_name]["model_version"],
+                model_name,
             ),
             "gene_median_path": self.model_dir
-            / model_version
+            / self.model_map[model_name]["model_version"]
             / "gene_median_dictionary.pkl",
-            "token_path": self.model_dir / model_version / "token_dictionary.pkl",
+            "token_path": self.model_dir
+            / self.model_map[model_name]["model_version"]
+            / "token_dictionary.pkl",
             "ensembl_dict_path": self.model_dir
-            / model_version
+            / self.model_map[model_name]["model_version"]
             / "ensembl_mapping_dict.pkl",
         }
 
@@ -137,7 +164,6 @@ class GeneformerConfig:
             "emb_layer": emb_layer,
             "emb_mode": emb_mode,
             "device": device,
-            "accelerator": accelerator,
             "input_size": self.model_map[model_name]["input_size"],
             "special_token": self.model_map[model_name]["special_token"],
             "embsize": self.model_map[model_name]["embsize"],
