d5153d49cf0a996a2c51b65b5bf8d45938630e8a
raschedh <rasched1997@gmail.com>
2025-09-09 02:58:02 +0200

    edited workflows and configs

--- examples/fine_tune_models/fine_tune_state.py
+++ examples/fine_tune_models/fine_tune_state.py
@@ -6,13 +6,40 @@ import scanpy as sc
 
 @hydra.main(version_base=None, config_path="../run_models/configs", config_name="state_config")
 def run_fine_tuning(cfg: DictConfig):
-    ann_data = sc.read_h5ad("./competition_support_set/competition_val_template.h5ad")
-    state_config = stateConfig()
-    state_model = stateFineTuningModel(configurer=state_config)
-    ann_data = state_model.process_data(ann_data)
-    state_model.train(ann_data)
-    state_model.predict()
-    return 
+
+    # Load the desired dataset
+    adata = sc.read_h5ad("competition_support_set/competition_val_template.h5ad")
+
+    # Get the desired label class
+    cell_types = list(adata.obs.cell_type)
+    label_set = set(cell_types)
+
+    # Create the fine-tuning model (no need to specify var_dims location)
+    config = stateConfig(
+        batch_size=8,
+        model_dir="competition/first_run",
+        model_config="configs/config.yaml",
+        freeze_backbone=True
+    )
+
+    model = stateFineTuningModel(
+        configurer=config, 
+        fine_tuning_head="classification", 
+        output_size=len(label_set),
+    )
+
+    # Process the data for training
+    data = model.process_data(adata)
+
+    # Create a dictionary mapping the classes to unique integers for training
+    class_id_dict = dict(zip(label_set, [i for i in range(len(label_set))]))
+
+    for i in range(len(cell_types)):
+        cell_types[i] = class_id_dict[cell_types[i]]
+
+    # Fine-tune
+    model.train(train_input_data=data, train_labels=cell_types)
+    return
     
 if __name__ == "__main__":
     run_fine_tuning()   
--- examples/run_models/run_state.py
+++ examples/run_models/run_state.py
@@ -11,43 +11,99 @@ import anndata as ad
 from datasets import load_dataset
 from helical.utils import get_anndata_from_hf_dataset
 import scanpy as sc
+from helical.models.state import vcc_eval
 
 @hydra.main(version_base=None, config_path="configs", config_name="state_config")
 def run_embeddings(cfg: DictConfig):
 
-    ann_data = sc.read_h5ad("./competition_support_set/competition_val_template.h5ad")
     state_config = stateConfig()
     state_model = stateEmbeddingsModel(configurer=state_config)
 
+    ann_data = sc.read_h5ad("./competition_support_set/competition_val_template.h5ad")
     ann_data = ann_data[:10].copy()
 
-    ann_data = state_model.process_data()
+    ann_data = state_model.process_data(ann_data)
     embeddings = state_model.get_embeddings(ann_data)
 
     print(embeddings.shape)
+    return
 
 
 def run_training(cfg: DictConfig):
-    ann_data = sc.read_h5ad("./competition_support_set/competition_val_template.h5ad")
-    train_config = trainingConfig()
-    state_model = stateTransitionTrainModel(configurer=train_config)
-    ann_data = state_model.process_data(ann_data)
-    state_model.train()
-    state_model.predict()
+
+    train_config = trainingConfig(
+        output_dir="competition",
+        name="first_run",
+        toml_config_path="competition_support_set/starter.toml",
+        checkpoint_name="final.ckpt",
+        max_steps=40000,
+        max_epochs=1,
+        ckpt_every_n_steps=20000,
+        num_workers=4,
+        batch_col="batch_var",
+        pert_col="target_gene",
+        cell_type_key="cell_type",
+        control_pert="non-targeting",
+        perturbation_features_file="competition_support_set/ESM2_pert_features.pt"
+        )
+
+    state_train = stateTransitionTrainModel(configurer=train_config)
+    state_train.train() 
+    state_train.predict() 
     return
 
 
 
 def run_inference(cfg: DictConfig):
-    ann_data = sc.read_h5ad("./competition_support_set/competition_val_template.h5ad")
-    state_config = stateConfig()
-    state_model = stateTransitionModel(configurer=state_config)
-    ann_data = state_model.process_data(ann_data)
-    state_model.predict()
+
+    state_config = stateConfig(
+        output = "competition/prediction.h5ad",
+        model_dir = "competition/first_run",
+        model_config = "configs/config.yaml",
+        pert_col = "target_gene",
+    )
+
+    adata = sc.read_h5ad("competition_support_set/competition_val_template.h5ad")
+
+    state_transition = stateTransitionModel(configurer=state_config)
+    adata = state_transition.process_data(adata)
+    embeds = state_transition.get_embeddings(adata)
     return
 
+def run_vcc_eval(cfg: DictConfig):
+    # default configs for competition dataset
+    EXPECTED_GENE_DIM = 18080
+    MAX_CELL_DIM = 100000
+    DEFAULT_PERT_COL = "target_gene"
+    DEFAULT_CTRL = "non-targeting"
+    DEFAULT_COUNTS_COL = "n_cells"
+    DEFAULT_CELLTYPE_COL = "celltype"
+    DEFAULT_NTC_NAME = "non-targeting"
+
+    configs = {
+        # path to the prediction file
+        "input": "competition/prediction.h5ad",
+        # path to the gene names file
+        "genes": "competition_support_set/gene_names.csv",
+        # path to the output file - if None will be created with default naming
+        "output": None,
+        "pert_col": DEFAULT_PERT_COL,
+        "celltype_col": None,
+        "ntc_name": DEFAULT_NTC_NAME,
+        "output_pert_col": DEFAULT_PERT_COL,
+        "output_celltype_col": DEFAULT_CELLTYPE_COL,
+        "encoding": 32,
+        "allow_discrete": False,
+        "expected_gene_dim": EXPECTED_GENE_DIM,
+        "max_cell_dim": MAX_CELL_DIM,
+    }
+
+    # this creates a submission file in the output directory which can be uploaded to the challenge leaderboard
+    vcc_eval(configs)
+    return 
 
 if __name__ == "__main__":
     run_embeddings()
     run_training()
     run_inference()
+    run_vcc_eval()
