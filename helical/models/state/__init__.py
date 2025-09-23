c37b3bfe238430bf3f790beb574f46a5e9f97acb
raschedh <rasched1997@gmail.com>
2025-09-23 14:40:45 +0200

    header notes

--- helical/models/state/__init__.py
+++ helical/models/state/__init__.py
@@ -1,3 +1,13 @@
+'''
+This package is based on STATE by Adduri et al., 
+available at https://www.biorxiv.org/content/10.1101/2025.06.26.661135v1. 
+Licensed under CC BY-NC-SA 4.0.  
+Modifications in this package: 
+- Updated API call wrappers 
+- Minor code restructuring
+
+'''
+
 import logging
 import sys
 
