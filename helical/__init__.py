d25d6b7d16ec0fff8a06d2dc5a866059685cff57
raschedh <141400232+raschedh@users.noreply.github.com>
2025-11-19 09:02:49 +0000

    Fix double logging +C2S Quantization (#302)
    * missing import and test for quantisation
    
    * fix double logging
    
    * add time and do not remove root loggers - not needed
    
    ---------
    
    Co-authored-by: Maxime Allard <maxime@helical-ai.com>
    Co-authored-by: Benoit Putzeys <157973952+bputzeys@users.noreply.github.com>

--- helical/__init__.py
+++ helical/__init__.py
@@ -3,18 +3,16 @@ import logging
 
 logging.captureWarnings(True)
 
-for handler in logging.root.handlers[:]:
-    logging.root.removeHandler(handler)
-
-logger = logging.getLogger()
+logger = logging.getLogger('helical')
 logger.setLevel(logging.INFO)
 
 handler = logging.StreamHandler()
 handler.setLevel(logging.INFO)
 
-formatter = logging.Formatter("%(levelname)s:%(name)s:%(message)s")
+formatter = logging.Formatter("%(asctime)s - %(levelname)s:%(name)s:%(message)s")
 handler.setFormatter(formatter)
 
 logger.addHandler(handler)
+logger.propagate = False  # Prevent propagation to root
 
 os.environ["TRANSFORMERS_VERBOSITY"] = "error"
