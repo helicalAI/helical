f798a965dedc3d2f6f01cf4e277d8ba73e4ebd47
bputzeys <benoit@helical-ai.com>
2026-03-24 13:11:47 +0100

    replaced AutoTokenizer/AutoModelForMaskedLM with
    direct imports of NicheformerTokenizer/NicheformerForMaskedLM, no trust_remote_code

--- helical/models/nicheformer/__init__.py
+++ helical/models/nicheformer/__init__.py
@@ -1,2 +1,7 @@
 from .model import Nicheformer
 from .nicheformer_config import NicheformerConfig
+from .modeling_nicheformer import (
+    NicheformerPreTrainedModel,
+    NicheformerModel,
+    NicheformerForMaskedLM,
+)
